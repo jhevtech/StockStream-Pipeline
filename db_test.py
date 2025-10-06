c4bd2f6824fa3c899cab6e0317574e3ba12795bb
Jhevaughn <135598706+jhevtech@users.noreply.github.com>
2025-10-06 04:14:03 -0700

    Delete db_test.py

--- db_test.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine, text
-engine = create_engine("postgresql+psycopg2://postgres:postgres@127.0.0.1:5433/market_pulse")
-with engine.connect() as conn:
-    v = conn.execute(text("SELECT version();")).scalar()
-    print("Connected to:", v)
